b94b7765c1d4dc4518a015c98f391087025b9b6f
Kajal Sharma <155152254+kajal7392@users.noreply.github.com>
2025-06-24 15:07:58 +0000

    Bruteforce approach solved

---
+++ Three_Sum_Array_Problem/BruteforceApproach.java
@@ -0,0 +1,64 @@
+/* Three Sum Problem in array:
+Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]] such that i != j, i != k,
+and j != k, and nums[i] + nums[j] + nums[k] == 0. Notice that the solution set must not contain 
+duplicate triplets.
+
+Example 1:
+
+Input: nums = [-1,0,1,2,-1,-4]
+Output: [[-1,-1,2],[-1,0,1]]
+Explanation: 
+nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+The distinct triplets are [-1,0,1] and [-1,-1,2].
+Notice that the order of the output and the order of the triplets does not matter.
+Example 2:
+
+Input: nums = [0,1,1]
+Output: []
+Explanation: The only possible triplet does not sum up to 0.
+Example 3:
+
+Input: nums = [0,0,0]
+Output: [[0,0,0]]
+Explanation: The only possible triplet sums up to 0.
+ 
+Constraints:
+3 <= nums.length <= 3000
+-105 <= nums[i] <= 105 */
+
+
+// Solution:
+public class BruteforceApproach {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};  //Example input
+        List <List<Integer>> result = threeSum(nums);
+
+        // Print the result
+        for(List<Integer> triplet : result) {
+            System.out.println(triplet);
+        }
+    }
+
+    public static List<List<Integer>> threeSum (int[] nums) {
+        List<List<Integer>> triplets = new ArrayList<>();
+        int n= nums.length;
+
+        // Bruteforce Approach
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                for(int k=j+1; k<n; k++) {
+                    if(nums[i]+ nums[j] + nums[k] == 0) {
+                        List<Integer> triplet = new ArrayList<>();
+                         triplet.add(nums[i]);
+                         triplet.add(nums[j]);
+                         triplet.add(nums[k]);
+                         triplets.add(triplet);
+                    }
+                }
+            }
+        }
+        return triplets;
+    }
+}
